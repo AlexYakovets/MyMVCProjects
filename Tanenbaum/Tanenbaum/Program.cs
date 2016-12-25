9f9762aaf045f7fa3ebdb2aa78afd0bd85837cfa
Alexandr Yakovets <375296904276@mail.ru>
2016-12-25 21:52:58 +0300

    added project tannenbaum for New 2017 Year

---
+++ Tanenbaum/Tanenbaum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tanenbaum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tannenbaum();
+        }
+        private static void Tannenbaum()
+        {
+            Console.Beep(247, 500);
+            Console.Beep(417, 500);
+            Console.Beep(417, 500);
+            Console.Beep(370, 500);
+            Console.Beep(417, 500);
+            Console.Beep(329, 500);
+            Console.Beep(247, 500);
+            Console.Beep(247, 500);
+            Console.Beep(247, 500);
+            Console.Beep(417, 500);
+            Console.Beep(417, 500);
+            Console.Beep(370, 500);
+            Console.Beep(417, 500);
+            Console.Beep(497, 500);
+            Thread.Sleep(500);
+            Console.Beep(497, 500);
+            Console.Beep(277, 500);
+            Console.Beep(277, 500);
+            Console.Beep(440, 500);
+            Console.Beep(440, 500);
+            Console.Beep(417, 500);
+            Console.Beep(370, 500);
+            Console.Beep(329, 500);
+            Console.Beep(247, 500);
+            Console.Beep(417, 500);
+            Console.Beep(417, 500);
+            Console.Beep(370, 500);
+            Console.Beep(417, 500);
+            Console.Beep(329, 500);
+        }
+    }
+}
