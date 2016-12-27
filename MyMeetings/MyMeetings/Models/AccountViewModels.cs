c710f20614dba03324cdd68a440c4ff3732bf5a0
Alexandr Yakovets <375296904276@mail.ru>
2016-12-27 23:00:23 +0300

    Validation OK. Dataformat in datePicker was changed

--- MyMeetings/MyMeetings/Models/AccountViewModels.cs
+++ MyMeetings/MyMeetings/Models/AccountViewModels.cs
@@ -70,14 +70,14 @@ namespace MyMeetings.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
         [Display(Name = "Дата Рождения")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DateOfBirth { get; set; }
 
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
         [Display(Name = "Пароль")]
         public string Password { get; set; }
 
