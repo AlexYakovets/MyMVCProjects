db14219737119687ede612c1effd126e63cbc8b0
Alexandr Yakovets <375296904276@mail.ru>
2017-01-03 21:35:57 +0300

    Edits in moderation controller

--- MyMeetings/MyMeetings/Controllers/ModerationController.cs
+++ MyMeetings/MyMeetings/Controllers/ModerationController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
@@ -18,5 +20,10 @@ namespace MyMeetings.Controllers
             var result = UserManager.Users;
             return View(result);
         }
+        public  ActionResult UserDetails(string id)
+        {
+            var result =   UserManager.Users.FirstOrDefault(user=>user.Id==id);
+            return PartialView(result);
+        }
     }
-}
+    }
