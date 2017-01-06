08c0aa8fd4c18aedf0d55d6c5467e6dd7812f26b
Alexander Yakovets <375296904276@mail.ru>
2017-01-06 18:28:18 +0300

    some edits

--- MyMeetings/MyMeetings/Scripts/CommonScripts/Roles/RoleAndUserhandler.js
+++ MyMeetings/MyMeetings/Scripts/CommonScripts/Roles/RoleAndUserhandler.js
@@ -5,9 +5,11 @@
         function displayRoles(data) {
             console.log(data);
             var role = JSON.parse(data);
-            $('#list-of-roles li').remove();
+            //$('#list-of-roles li').remove();
+            $('#list-of-roles input').remove();
             role.forEach(function(elem) {
-                $("#list-of-roles").append("<li><input type='checkbox' data-userId="+elem.userId+" name='roles' " + (elem.IsAvaible ? "checked" : " ") + " value=" + elem.Role.Id + ">" + elem.Role.Name + "</li>");
+                //$("#list-of-roles").append("<li><input type='checkbox' data-userId="+elem.userId+" name='roles' " + (elem.IsAvaible ? "checked" : " ") + " value=" + elem.Role.Id + ">" + elem.Role.Name + "</li>");
+                $("#list-of-roles").append("<input type='checkbox' data-userId=" + elem.userId + " name='roles' " + (elem.IsAvaible ? "checked" : " ") + " value=" + elem.Role.Id + ">" + elem.Role.Name + "&nbsp" + "&nbsp" + "&nbsp" + "&nbsp" + "&nbsp" + "&nbsp" + "&nbsp" + "&nbsp");
             });
 
         }
