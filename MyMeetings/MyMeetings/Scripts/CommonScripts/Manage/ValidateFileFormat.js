7a87e27050d2e5270c80ce7d65e2832bb66868fe
Alexander Yakovets <375296904276@mail.ru>
2017-01-12 16:59:20 +0300

    work commit

--- MyMeetings/MyMeetings/Scripts/CommonScripts/Manage/ValidateFileFormat.js
+++ MyMeetings/MyMeetings/Scripts/CommonScripts/Manage/ValidateFileFormat.js
@@ -2,7 +2,15 @@
     $("#upload-image-button").click(function(e) {
         var inputFile = document.getElementById("imagefile").files;
         var file = inputFile.item(0);
-        file.type == 'image/png' ? console.log("Все ОК") : alert("Файл не того формата");
+        file.type == "image/png" ? OnSucces() : onFailed(e);
 
+        function OnSucces() {
+            $(".status-message").append("<div class='alert alert-success'>File will be uploaded to the server </div>");
+
+        }
+        function onFailed(e) {
+            $(".status-message").append("<div class='alert alert-warning'>File is not have image extension </div>");
+            e.preventDefault();
+        }
     });
 });
