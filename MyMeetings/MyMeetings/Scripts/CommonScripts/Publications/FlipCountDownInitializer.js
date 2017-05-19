92d5bdf3f2cc6fb5aa2b9d9175889b0f2d2bebcd
Alexander Yakovets <375296904276@mail.ru>
2017-05-19 19:05:57 +0300

    finall work commit

--- MyMeetings/MyMeetings/Scripts/CommonScripts/Publications/FlipCountDownInitializer.js
+++ MyMeetings/MyMeetings/Scripts/CommonScripts/Publications/FlipCountDownInitializer.js
@@ -1,15 +1,29 @@
 ﻿
 $(function () {
-    $('.timer').each(function (index, element)
-    {
-        var date = $('#' + element.id + ' .DateTimeOfMeet').val;
-        Console.log(date);
-    })
-    var dt = new Date();
-    $('.timer').flipcountdown({
-        size: "sm",
-        //beforeDateTime: ('05/01/2017 00:00:01')
-        beforeDateTime: $('.timer .DateTimeOfMeet')
-        //beforeDateTime: '1/01/' + (dt.getFullYear() + 1) + ' 00:00:01'
-    });
+    var elements = document.getElementsByClassName("timer")
+    for (var i = 0; i < elements.length; i++) {
+        var timer = elements[i];
+        var id = timer.getAttribute("value");
+        var date = document.getElementById(id).getAttribute("value");
+        $(timer).flipcountdown({
+            size: "sm",
+            beforeDateTime: date
+
+            //beforeDateTime: '1/01/' + (dt.getFullYear() + 1) + ' 00:00:01'
+        });
+        //    console.log(date);
+        //}
+        //{
+        //    console.log(element);
+        //    var date = $('#PublicationId[value=' + element + '] .DateTimeOfMeet');
+        //    console.log(date);
+        //}
+        //var dt = new Date();
+        //$('.timer').flipcountdown({
+        //    size: "sm",
+        //    //beforeDateTime: ('05/01/2017 00:00:01')
+
+        //    //beforeDateTime: '1/01/' + (dt.getFullYear() + 1) + ' 00:00:01'
+        //});
+    }
 });
